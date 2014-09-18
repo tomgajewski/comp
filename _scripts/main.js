8716ef912fc42f4527e9dc3eceabbdd5ffa30d1e
Tom <tom@Toms-MacBook.local>
2014-09-18 19:45:09 +0200

    some cleaining

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,14 +1,35 @@
 $(function() {
-  if($('#slideshow .slider').length) {
-    $('#slideshow .slider').bxSlider({
-      adaptiveHeight: true,
-      pager: true
-    });
-  }
-  
-  $('#menu-toggle').on('click', function(e) {
-    e.preventDefault();
-    
-    $('#navigation').slideToggle('normal');
-  });
+
+
+  /** trigger offcanvas nav on devices below 840px wide **/
+    $("#nav-trigger").click(function() {
+      $(".offcanvas-navigation").html('');
+      $(".offcanvas-navigation").append('<ul></ul>');
+      $(".offcanvas-navigation ul").html($("#navigation ul").html().replace('<br>',''));
+      $(".offcanvas-navigation ul li").addClass("nav-item");
+    })
+  /** end **/
+
+
+  /** slider, fixing description container position **/
+    var width = $( window ).width();
+    if(width < 650)
+    {
+      var slider_height = 0;
+      slider_height += parseInt($(".slide-image").height());
+      $('#slides').slidesjs({
+        width: 400,
+        height: slider_height,
+      });
+    }
+    else
+    {
+      $('#slides').slidesjs({
+         width:400,
+         height:200,
+      });
+    }
+  /** end **/
+
+
 });
